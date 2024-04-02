0e436be9d4bccc54038aaeaef6129930b3bd12cb
TinsFox <fox@tinsfox.com>
2024-04-02 15:52:33 +0800

    fix: suppressHydrationWarning

--- apps/remix/app/root.tsx
+++ apps/remix/app/root.tsx
@@ -9,14 +9,14 @@ import "./app.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
         <ScrollRestoration />
         <Scripts />
