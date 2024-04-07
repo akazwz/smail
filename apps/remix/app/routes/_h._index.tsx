df6dbdae2761324fa58468df2e7259fe303378dd
TinsFox <fox@tinsfox.com>
2024-04-07 23:29:22 +0800

    fix: cf turnstile check

--- apps/remix/app/routes/_h._index.tsx
+++ apps/remix/app/routes/_h._index.tsx
@@ -7,24 +7,29 @@ import { turnstileCheck, Account, defaultLayout } from "./_h";
 
 export const action: ActionFunction = async ({ request }) => {
   try {
+    const formData = await request.formData();
+    const cfTurnstileResponse = formData.get("cf-turnstile-response");
+
+    const headers = request.headers.get("Cookie");
+    const userName = formData.get("userName");
+    const domain = formData.get("domain");
     const TURNSTILE_ENABLED = process.env.TURNSTILE_ENABLED === "true";
+
     if (TURNSTILE_ENABLED) {
-      const passed = await turnstileCheck(request);
+      const passed = await turnstileCheck(cfTurnstileResponse as string);
+
       if (!passed) {
         return {
           error: "Failed to pass the turnstile",
         };
       }
     }
-    const formData = await request.formData();
-    const headers = request.headers.get("Cookie");
-    const userName = formData.get("userName");
-    const domain = formData.get("domain");
 
     const oldAccountList: Account[] =
       (await accountListCookie.parse(headers)) || [];
 
     const isExisting = oldAccountList.find((m) => m.userName === userName);
+
     if (isExisting) {
       return redirect("/");
     } else {
@@ -37,6 +42,7 @@ export const action: ActionFunction = async ({ request }) => {
 
       const newAccountList = await accountListCookie.serialize(oldAccountList);
       const currentAccount = await currentAccountCookie.serialize(emailAddress);
+
       return redirect("/", {
         headers: [
           ["Set-Cookie", newAccountList],
--- apps/remix/app/routes/_h.tsx
+++ apps/remix/app/routes/_h.tsx
@@ -64,20 +64,18 @@ export const loader: LoaderFunction = async ({ request }) => {
   };
 };
 
-export async function turnstileCheck(request: Request): Promise<boolean> {
-  const response = (await request.formData()).get("cf-turnstile-response");
-  if (!response) {
+export async function turnstileCheck(cfCode: string): Promise<boolean> {
+  if (!cfCode) {
     return false;
   }
   const verifyEndpoint = process.env
     .CLOUDFLARE_TURNSTILE_VERIFY_Endpoint as string;
-
   const secret = process.env.TURNSTILE_SECRET;
   const resp = await fetch(verifyEndpoint, {
     method: "POST",
     body: JSON.stringify({
       secret,
-      response,
+      response: cfCode,
     }),
     headers: {
       "Content-Type": "application/json",
